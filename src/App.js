0cf566aa1c89ec6f5fce0687ecaf894e16358e1e
DESKTOP-NPJ9KD5\DK <khoado.dk@gmail.com>
2022-05-30 11:25:07 -0400

    Setting up folder structures and general stylings

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import "./App.css";
-
-import { useContext } from "react";
-import { themeContext } from "./Context";
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Experience from './components/Experience/Experience'
+import Services from './components/Services/Services'
+import Portfolio from './components/Portfolio/Portfolio'
+import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const theme = useContext(themeContext);
   return (
-    <div
-      className="App"
-    >
-      <h1>Hello from react</h1>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ThemeProvider } from './Context';
+
+import './index.css'
 
 ReactDOM.render(
-  <ThemeProvider>
+  <>
     <App />
-  </ThemeProvider>,
+  </>,
   document.getElementById('root')
 );
