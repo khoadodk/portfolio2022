2aa0248bc3dc2dd63de0bb7b7178b5385ffda70a
unknown <khoado.dk@gmail.com>
2022-08-01 14:15:12 -0400

    Added google analytic

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className="cta">
         <a href='https://www.dropbox.com/scl/fi/028q88uiwl7sqvd1q97da/KD-resume.docx?dl=0&rlkey=dqb7ewifqggoeuyv8j4m89qmh' target="__blank" className='btn'>Resume</a>
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        <a href='#contact' className='btn btn-primary'>Hire me</a>
     </div>
   )
 }
