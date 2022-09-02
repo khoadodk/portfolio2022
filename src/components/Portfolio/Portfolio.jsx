c117a81dfa6f74a194b9fca679fb4df325c98f06
unknown <khoado.dk@gmail.com>
2022-09-02 11:01:21 -0400

    Added colorme woocommerce project

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -11,9 +11,17 @@ import IMG10 from '../../assets/portfolio10.png';
 import IMG11 from '../../assets/portfolio11.png';
 import IMG12 from '../../assets/portfolio12.png';
 import IMG13 from '../../assets/portfolio13.png';
-// import IMG14 from '../../assets/portfolio14.png';
+import IMG15 from '../../assets/portfolio15.png';
 
 const data = [
+  {
+    id: 15,
+    image: IMG15,
+    title: 'WooCommerce Theme',
+    github: 'https://github.com/khoadodk/color-me',
+    demo: 'http://colorme.beauty',
+    category: 'wordpress',
+  },
   {
     id: 11,
     image: IMG11,
